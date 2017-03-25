53b7e297b96a40d3c1cda0c3a8cf6d8fb163ed15
jdbrice <jdb12@rice.edu>
2017-03-25 13:28:49 -0500

    Initialize values to 0

--- PythiaTrack.h
+++ PythiaTrack.h
@@ -6,7 +6,22 @@
 class PythiaTrack : public TObject {
 	
 public:
-	PythiaTrack() {}
+	PythiaTrack() {
+		Int_t   mId = 0.0;
+		Float_t mEnergy = 0.0;
+		Int_t   mKF = 0.0;
+		Int_t   mKS = 0.0;
+		Float_t mLifetime = 0.0;
+		Float_t mMass = 0.0;
+		Int_t   mParent = 0.0;
+		Float_t mPx = 0.0;
+		Float_t mPy = 0.0;
+		Float_t mPz = 0.0;
+		Float_t mTime = 0.0;
+		Float_t mVx = 0.0;
+		Float_t mVy = 0.0;
+		Float_t mVz = 0.0;
+	}
 	virtual ~PythiaTrack() {}
 
 
